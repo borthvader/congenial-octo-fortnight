bbe10a47f4c7b8c52135e148b250b4aa425b500b
Borthvader <104023232+borthvader@users.noreply.github.com>
2022-09-12 14:51:59 -0400

    created beginning of prompts for user

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,55 @@
 // TODO: Include packages needed for this application
+const inquirer =require('inquirer');
+const fs = require('fs');
+const {generateMarkdown} = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([{
+        name: 'license',
+        message: 'Select the type of license for this application:',
+        type: 'list',
+        choices: ['MIT', 'ISC', 'GNU LGPL', 'Unlicense', 'none'],
+        default: 'MIT'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What would you like to title the project? (Required)',
+        validate: function(name){
+            if (name){
+                return true;
+            } else {
+                console.log('You must have a project title!')
+                return false;
+            }
+            }
+        
+    },{
+        type: 'input',
+        name: 'user',
+        message: 'What is your username for GitHub? (Required)',
+        validate: function(name){
+            if (name){
+                return true;
+            } else{
+                console.log('You must enter your username!')
+                return false;
+            }
+        }    
+    },{
+        type: 'input',
+        name: 'description',
+        message: 'Give some info about your project'
+    },{
+        type: 'input',
+        name: 'install',
+        message: 'Instructions for the installation of this project'
+    }, {
+        
+    }
+
+    ])};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
